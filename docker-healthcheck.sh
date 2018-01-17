86397c10b2e125708cd575e2983dbc7688920e49
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2018-01-17 17:21:54 +0000

    Updated healthcheck and code-content-command

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --fail -H 'Accept: pson' \
+curl -s --fail -H 'Accept: pson' \
 --resolve 'puppet:8140:127.0.0.1' \
 --cert   $(puppet config print hostcert) \
 --key    $(puppet config print hostprivkey) \
